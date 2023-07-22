a0d5449b34dedd80fb16b2d571d23995908cae9c
black-august <i@cantreally.cyou>
2023-07-21 22:13:43 -0400

    add fzf

--- install.sh
+++ install.sh
@@ -12,12 +12,13 @@ else
 fi
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#git clone https://github.com/joshskidmore/zsh-fzf-history-search ~/.oh-my-zsh/plugins/zsh-fzf-history-search
+git clone https://github.com/joshskidmore/zsh-fzf-history-search ~/.oh-my-zsh/plugins/zsh-fzf-history-search
 
-cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
 cp .vimrc ~/.vimrc
 
 chsh -s $(which zsh)
+
+cp .zshrc ~/.zshrc
 
 echo "All done..."
