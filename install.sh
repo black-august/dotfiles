39190b1d4f23003430e1da84f44d7854267cbbda
black-august <i@cantreally.cyou>
2023-11-18 22:53:35 -0500

    adding shit

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
+function install_zsh_and_plugins () {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  git clone https://github.com/joshskidmore/zsh-fzf-history-search ~/.oh-my-zsh/plugins/zsh-fzf-history-search || echo "Failed to install fzf history search"
+}
+
+function install_vim_plugins () {
+  mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+  git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree || echo "Failed to install nerdtree"
+  git clone https://github.com/lilydjwg/colorizer ~/.vim/bundle || echo "Failed to install colorizer"
+}
+
+function install_dotfiles () {
+  cp .tmux.conf ~/.tmux.conf
+  cp .vimrc ~/.vimrc
+  cp .zshrc ~/.zshrc
+  cp -r .config/ ~/.config
+}
+
 if command -v apt-get &> /dev/null; then
     sudo apt-get update -y
     sudo apt-get install -y fzf git tmux vim ripgrep curl \
@@ -11,15 +29,9 @@ else
     exit 1
 fi
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/joshskidmore/zsh-fzf-history-search ~/.oh-my-zsh/plugins/zsh-fzf-history-search
-
-cp .tmux.conf ~/.tmux.conf
-cp .vimrc ~/.vimrc
-cp -r .config/ ~/.config
+install_zsh_and_plugins
+install_vim_plugins
+install_dotfiles
 
 chsh -s $(which zsh)
-
-cp .zshrc ~/.zshrc
-
 echo "All done..."
